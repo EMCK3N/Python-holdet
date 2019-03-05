526e7236610a1690027884265979f99cbafa2be1
Michael Fogh Emcken <michaelemcken@Michaels-MacBook-Pro.local>
2019-03-05 16:17:16 +0100

    Clean up

--- Tommy er klar.py
+++ Tommy er klar.py
@@ -1,9 +1,7 @@
 #Denne fil er har kun et formål og det er at gøre Tommy glad når han får den ned med git:sync
 
-message = "mission complete"
-print(message)
-print(message*4)
-print(message*3)
+message = "Mission complete sir."
 
+print(message)
 
 
